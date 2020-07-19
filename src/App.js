39ec0eaeafcea889ab9dbe219b2a716ad31d4f5d
Ehsan <ehsan@user12s-MacBook-Pro.local>
2020-07-19 12:53:22 +0200

    Sign In form with styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import HomePage from './Pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './Pages/shop/shop.component';
+import Header from './Components/header/header.component';
+import SignInAndSignUpPage from './Pages/sign-in-sign-out/sign-in-sign-out.component'
 
 function App() {
   return (
     <div >
+    <Header />
     <Switch>
     <Route exact path='/' component={HomePage}/>
     <Route  path='/shop' component={ShopPage}/>
+    <Route path='/signin' component={SignInAndSignUpPage}/>
     </Switch>
     </div>
   );
