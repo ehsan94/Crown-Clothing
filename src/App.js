799266201afc7866199a4bed5482fd2a7ff6384d
Ehsan <ehsan@user12s-MacBook-Pro.local>
2020-07-20 15:45:30 +0200

    Sign in functionality

--- src/App.js
+++ src/App.js
@@ -5,25 +5,39 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from "./Pages/shop/shop.component";
 import Header from "./Components/header/header.component";
 import SignInAndSignUpPage from "./Pages/sign-in-sign-out/sign-in-sign-out.component";
-import { auth } from './Components/firebase/firebase.utils';
-
+import {
+  auth,
+  createUserProfileDocument,
+} from "./Components/firebase/firebase.utils";
 
 class App extends React.Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      currentUser : null
-    }
+      currentUser: null,
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              }
+            });
+           // console.log(this.state);
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/Pages/sign-in-sign-out/sign-in-sign-out.component.jsx
+++ src/Pages/sign-in-sign-out/sign-in-sign-out.component.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import SignIn from '../../Components/sign-in/sign-in.component';
+import SignIn from "../../Components/sign-in/sign-in.component";
+import SignUp from "../../Components/sign-up/sign-up.component";
 
-const SignInAndSignUpPage = () =>(
-    <div className='sign-in-and-sign-up'>
-    <SignIn/>
-    </div>
-)
+import "./sign-in-sign-out.styles.scss";
+
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp/>
+  </div>
+);
 
 export default SignInAndSignUpPage;
