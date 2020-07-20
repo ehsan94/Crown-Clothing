a8ad78348264c022a87fbb521bc017b2d2bb95e2
Ehsan <ehsan@user12s-MacBook-Pro.local>
2020-07-20 18:37:00 +0200

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+
+import store from './redux/store'
+import { Provider } from "react-redux";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider  store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
---
+++ src/redux/user/user.actions.js
@@ -0,0 +1,4 @@
+export const exportCurrentUser = user => ({
+    type:'SET_CURRENT_USER',
+    payload:user
+});
